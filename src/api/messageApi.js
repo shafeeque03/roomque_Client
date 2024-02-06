54b2681be59fefa8e5063215590d3bddd5a0fd9a
shafeeque03 <129934317+shafeeque03@users.noreply.github.com>
2024-02-06 21:56:16 +0530

    mongoUrl Updated

--- src/api/messageApi.js
+++ src/api/messageApi.js
@@ -1,5 +1,5 @@
 import axios from "axios";
-const baseURL = "https://roomque-server.onrender.com";
+const baseURL = "https://roomque-backend.onrender.com";
 const messageInstance  = axios.create({baseURL:baseURL})
 
 export const getMessages = (id) => messageInstance.get(`/message/${id}`)
